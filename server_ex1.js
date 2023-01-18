79692b7f84a493ce14da09e261f889795b579ec1
Sandiecodes <99297260+Sandiecodes@users.noreply.github.com>
2023-01-18 09:30:07 -0600

    Add files via upload

---
+++ server_ex1.js
@@ -0,0 +1,15 @@
+'use strict';
+// load package
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const app = express();
+
+app.get('/greeting', (req,res) => {
+res.send('hello ');
+});
+// app.use('/', express.static('pages'));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
